d0272550661c914a1e14a237f3654dd5eac2c205
Sofia Levin <sofiafontes@me.com>
2020-06-15 17:08:37 -0700

    Small update.

--- database/seeds/009-avatars.js
+++ database/seeds/009-avatars.js
@@ -5,8 +5,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: "us-west-1",
-  "accessKeyId": process.env.AWS_ACCESS_KEY_ID, 
-  "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const s3 = new AWS.S3();
